3f0d0e6505f3ce1271e5c5bf110394b5a2d079db
csurajit07335 <csuraj07335@gmail.com>
2023-10-12 21:27:38 +0530

    fffdwerwerwr

--- src/main/java/com/mkyong/Application.java
+++ src/main/java/com/mkyong/Application.java
@@ -14,73 +14,10 @@ import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-    //https://docs.spring.io/spring/docs/5.1.6.RELEASE/spring-framework-reference/integration.html#mail
-    @Autowired
-    private JavaMailSender javaMailSender;
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        System.out.println("Hello, World! er This is a Baeldung ffnow Spring Native Application");
     }
 
-    @Override
-    public void run(String... args) {
-
-        System.out.println("Sending Email...");
-
-        try {
-            //sendEmail();
-            sendEmailWithAttachment();
-
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Done");
-
-    }
-
-    void sendEmail() {
-
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo("1@gmail.com", "2@yahoo.com");
-
-        msg.setSubject("Testing from Spring Boot");
-        msg.setText("Hello World \n Spring Boot Email");
-
-        javaMailSender.send(msg);
-
-    }
-
-    void sendEmailWithAttachment() throws MessagingException, IOException {
-
-        MimeMessage msg = javaMailSender.createMimeMessage();
-
-        // true = multipart message
-        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
-        helper.setTo("1@gmail.com");
-
-        helper.setSubject("Testing from Spring Boot");
-
-        // default = text/plain
-        //helper.setText("Check attachment for image!");
-
-        // true = text/html
-        helper.setText("<h1>Check attachment for image!</h1>", true);
-
-        //FileSystemResource file = new FileSystemResource(new File("classpath:android.png"));
-
-        //Resource resource = new ClassPathResource("android.png");
-        //InputStream input = resource.getInputStream();
-
-        //ResourceUtils.getFile("classpath:android.png");
-
-        helper.addAttachment("my_photo.png", new ClassPathResource("android.png"));
-
-        javaMailSender.send(msg);
-
-    }
 }
